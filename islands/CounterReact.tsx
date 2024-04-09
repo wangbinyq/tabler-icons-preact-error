96cca0f76625520fd7d210dff217986d643a5c6f
wangb <wangbinyq@gmail.com>
2024-04-09 09:44:20 +0800

    init

---
+++ islands/CounterReact.tsx
@@ -0,0 +1,23 @@
+import type { Signal } from "@preact/signals";
+import { Button } from "../components/Button.tsx";
+import { IconArrowDown, IconArrowUp } from "@tabler/icons-react";
+
+interface CounterProps {
+  count: Signal<number>;
+}
+
+export default function CounterReact(props: CounterProps) {
+  return (
+    <div class="flex gap-8 py-6">
+      <Button onClick={() => props.count.value -= 1}>
+        <IconArrowDown />
+        -1
+      </Button>
+      <p class="text-3xl tabular-nums">{props.count}</p>
+      <Button onClick={() => props.count.value += 1}>
+        <IconArrowUp />
+        +1
+      </Button>
+    </div>
+  );
+}
---
+++ routes/index.tsx
@@ -0,0 +1,33 @@
+import { useSignal } from "@preact/signals";
+import CounterReact from "../islands/CounterReact.tsx";
+import CounterPreact from "../islands/CounterPreact.tsx";
+
+export default function Home() {
+  const count = useSignal(3);
+  return (
+    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+        <img
+          class="my-6"
+          src="/logo.svg"
+          width="128"
+          height="128"
+          alt="the Fresh logo: a sliced lemon dripping with juice"
+        />
+        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <p class="my-4">
+          Try updating this message in the
+          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+        </p>
+        <h2>
+          React OK
+        </h2>
+        <CounterReact count={count} />
+        <h2>
+          Preact Error
+        </h2>
+        <CounterPreact count={count} />
+      </div>
+    </div>
+  );
+}
